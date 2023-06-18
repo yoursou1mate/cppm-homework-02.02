dc456ae2122b887876a9290fb56deeaf0cd90c55
a1ex.solovyov <a1ex.solovyov10@gmail.com>
2023-06-18 22:04:22 +0300

    Second commit

--- cppm-homework-02.02/main.cpp
+++ cppm-homework-02.02/main.cpp
@@ -6,9 +6,47 @@
 //
 
 #include <iostream>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+struct BankAccount
+{
+    int accountNumber;
+    std::string name;
+    int balance;
+};
+
+void NewBalance (struct BankAccount& Person)
+{
+    int newBalance = 0;
+    std::cout << "Please enter your new balance: ";
+    std::cin >> newBalance;
+    std::cout << '\n';
+    Person.balance = newBalance;
+}
+
+
+
+int main(int argc, const char * argv[])
+{
+  
+
+        BankAccount Person;
+        
+        std::cout << "Please enter your account number: ";
+        std::cin >> Person.accountNumber;
+        std::cout << '\n';
+        std::cout << "Please the owner's name: ";
+        std::cin >> Person.name;
+        std::cout << '\n';
+        std::cout << "Please enter your current balance: ";
+        std::cin >> Person.balance;
+        std::cout << '\n';
+        
+    NewBalance(Person);
+    
+    std::cout << "Your account is: " << Person.accountNumber  << ", " << Person.name << ", " << Person.balance << std::endl;
+
+        
+        
     return 0;
 }
